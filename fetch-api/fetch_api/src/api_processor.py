5c96e111bc9ba3c4deb7c27093c2ba2f7178950d
Martin Stanchev <skullfighter6@gmail.com>
2025-11-11 20:51:52 +0200

    fetch-api: Introduce APIProcessor which reduces code duplication

---
+++ fetch-api/fetch_api/src/api_processor.py
@@ -0,0 +1,82 @@
+import json
+from common.utils.web import create_session
+from common.messages.api import client_responses
+from common.telemetry.src.tracing.wrappers import traced
+from common.telemetry.src.tracing.helpers import reword
+from fetch_api.settings import (settings, connectors)
+from fetch_api.src.telemetry.logging import log
+from fetch_api.src.client import ConnectorClient
+from fastapi.responses import JSONResponse
+
+
+
+class APIProcessor:
+    @staticmethod
+    @traced()
+    def process_response(client: ConnectorClient, request, endpoint: str, upstream_endpoint: str, ai_prompt: str, span=None):
+        try:
+            result = client.get(upstream_endpoint)
+            
+            assert result.status_code == 200
+            query_result = result.json()
+
+            log.debug('Fetch completed', extra={
+                'connector': 'grafana',
+                'endpoint': endpoint,
+                'upstream_endpoint': upstream_endpoint
+            })
+
+            if request.ai:
+                if 'ml' in connectors:
+                    try:
+                        session = create_session(timeout=180)
+                        response = session.post(
+                            f'http://{settings.listen_host}:{settings.listen_port}/ml/ask',
+                            headers={'Content-Type': 'application/json'},
+                            json={
+                                'instructions_template': 'default',
+                                'prompt': '{}\n\n{}'.format(
+                                    ai_prompt,
+                                    json.dumps(query_result['items'])
+                                )
+                            }
+                        )
+
+                        assert response.status_code == 200
+                        query_result['ai_summary'] = response.json()['items'][0]
+
+                        log.debug('Fetched AI summary', extra={
+                            'connector': 'grafana',
+                            'endpoint': endpoint,
+                            'ml_endpoint': '/ml/ask',
+                            'upstream_endpoint': upstream_endpoint
+                        })
+
+                    except Exception as ai_err:
+                        log.warning('AI summary fetch failed', extra={
+                            'connector': 'grafana',
+                            'endpoint': endpoint,
+                            'ml_endpoint': '/ml/ask',
+                            'upstream_endpoint': upstream_endpoint,
+                            'error': str(ai_err)
+                        })
+
+                else:
+                    log.warning('Skipping AI processing, as ML connector is not enabled', extra={
+                        'connector': 'grafana',
+                        'endpoint': endpoint,
+                        'ml_endpoint': '/ml/ask',
+                        'upstream_endpoint': upstream_endpoint
+                    })
+
+            return JSONResponse(content=query_result, status_code=200)
+
+        except Exception as err:
+            log.error('Fetch failed', extra={
+                'connector': 'grafana',
+                'endpoint': endpoint,
+                'upstream_endpoint': upstream_endpoint,
+                'error': str(err)
+            })
+
+            return JSONResponse(content=client_responses['upstream-error'], status_code=502)
--- fetch-api/fetch_api/src/routes/grafana.py
+++ fetch-api/fetch_api/src/routes/grafana.py
@@ -1,11 +1,7 @@
-import json
-from common.utils.web import create_session
-from common.messages.api import client_responses
-from fetch_api.settings import (settings, connectors)
-from fetch_api.src.telemetry.logging import log
+from fetch_api.settings import connectors
 from fetch_api.src.client import ConnectorClient
+from fetch_api.src.api_processor import APIProcessor
 from fastapi import APIRouter
-from fastapi.responses import JSONResponse
 
 from fetch_api.src.schemas.grafana import GrafanaRequest
 
@@ -16,139 +12,21 @@ client = ConnectorClient(connectors['grafana'].name)
 
 @router.post('/argocd-apps')
 def fetch_argocd_apps(request: GrafanaRequest):
-    try:
-        result = client.get('prometheus/argocd-apps')
-
-        assert result.status_code == 200
-        query_result = result.json()
-
-        log.debug('Fetch completed', extra={
-            'connector': 'grafana',
-            'endpoint': '/grafana/argocd-apps',
-            'destination_endpoint': '/prometheus/argocd-apps'
-        })
-
-        if request.ai:
-            if 'ml' in connectors:
-                try:
-                    session = create_session(timeout=180)
-                    response = session.post(
-                        f'http://{settings.listen_host}:{settings.listen_port}/ml/ask',
-                        headers={'Content-Type': 'application/json'},
-                        json={
-                            'instructions_template': 'default',
-                            'prompt': '{}\n\n{}'.format(
-                                'How are my argocd apps doing?',
-                                json.dumps(query_result['items'])
-                            )
-                        }
-                    )
-
-                    assert response.status_code == 200
-                    query_result['ai_summary'] = response.json()['items'][0]
-
-                    log.debug('Fetched AI summary', extra={
-                        'connector': 'grafana',
-                        'endpoint': '/grafana/argocd-apps',
-                        'ai_endpoint': '/ml/ask',
-                        'destination_endpoint': '/prometheus/argocd-apps'
-                    })
-
-                except Exception as ai_err:
-                    log.warning('AI summary fetch failed', extra={
-                        'connector': 'grafana',
-                        'endpoint': '/grafana/argocd-apps',
-                        'ai_endpoint': '/ml/ask',
-                        'destination_endpoint': '/prometheus/argocd-apps',
-                        'error': str(ai_err)
-                    })
-
-            else:
-                log.warning('Skipping AI processing, as ML connector is not enabled', extra={
-                    'connector': 'grafana',
-                    'endpoint': '/grafana/argocd-apps',
-                    'ai_endpoint': '/ml/ask',
-                    'destination_endpoint': '/prometheus/argocd-apps'
-                })
-
-        return JSONResponse(content=query_result, status_code=200)
-
-    except Exception as err:
-        log.error('Fetch failed', extra={
-            'connector': 'grafana',
-            'endpoint': '/grafana/argocd-apps',
-            'destination_endpoint': '/prometheus/argocd-apps',
-            'error': str(err)
-        })
-
-        return JSONResponse(content=client_responses['upstream-error'], status_code=502)
+    return APIProcessor.process_response(
+        client=client,
+        request=request,
+        endpoint='/grafana/argocd-apps',
+        upstream_endpoint='prometheus/argocd-apps',
+        ai_prompt='How are my ArgoCD apps doing?'
+    )
 
 
 @router.post('/car-battery')
 def fetch_car_battery(request: GrafanaRequest):
-    try:
-        result = client.get('postgresql/car-battery')
-
-        assert result.status_code == 200
-        query_result = result.json()
-
-        log.debug('Fetch completed', extra={
-            'connector': 'grafana',
-            'endpoint': '/grafana/car-battery',
-            'destination_endpoint': '/postgresql/car-battery'
-        })
-
-        if request.ai:
-            if 'ml' in connectors:
-                try:
-                    session = create_session(timeout=180)
-                    response = session.post(
-                        f'http://{settings.listen_host}:{settings.listen_port}/ml/ask',
-                        headers={'Content-Type': 'application/json'},
-                        json={
-                            'instructions_template': 'default',
-                            'prompt': '{}\n\n{}'.format(
-                                'How much battery does my Tesla have, do I need to charge soon?',
-                                json.dumps(query_result['items'])
-                            )
-                        }
-                    )
-
-                    assert response.status_code == 200
-                    query_result['ai_summary'] = response.json()['items'][0]
-
-                    log.debug('Fetched AI summary', extra={
-                        'connector': 'grafana',
-                        'endpoint': '/grafana/car-battery',
-                        'ai_endpoint': '/ml/ask',
-                        'destination_endpoint': '/postgresql/car-battery'
-                    })
-
-                except Exception as ai_err:
-                    log.warning('AI summary fetch failed', extra={
-                        'connector': 'grafana',
-                        'endpoint': '/grafana/car-battery',
-                        'ai_endpoint': '/ml/ask',
-                        'destination_endpoint': '/postgresql/car-battery',
-                        'error': str(ai_err)
-                    })
-
-            else:
-                log.warning('Skipping AI processing, as ML connector is not enabled', extra={
-                    'connector': 'grafana',
-                    'endpoint': '/grafana/car-battery',
-                    'ai_endpoint': '/ml/ask',
-                    'destination_endpoint': '/postgresql/car-battery'
-                })
-
-        return JSONResponse(content=query_result, status_code=200)
-
-    except Exception as err:
-        log.error('Fetch failed', extra={
-            'connector': 'grafana',
-            'endpoint': '/grafana/car-battery',
-            'destination_endpoint': '/postgresql/car-battery',
-            'error': str(err)
-        })
-
-        return JSONResponse(content=client_responses['upstream-error'], status_code=502)
+    return APIProcessor.process_response(
+        client=client,
+        request=request,
+        endpoint='/grafana/car-battery',
+        upstream_endpoint='postgresql/car-battery',
+        ai_prompt='How much battery does my Tesla have, do I have to charge soon?'
+    )
