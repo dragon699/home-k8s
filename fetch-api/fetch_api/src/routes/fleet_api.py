a1a488275dd0058ed0023c0d481f5781a13493b6
Martin Stanchev <skullfighter6@gmail.com>
2025-11-19 00:26:47 +0200

    fetch-api: (Chore) Experiment with Tesla Fleet API

--- fetch-api/fetch_api/src/routes/fleet_api.py
+++ fetch-api/fetch_api/src/routes/fleet_api.py
@@ -7,7 +7,7 @@ router = APIRouter()
 
 @router.get('/.well-known/appspecific/com.tesla.3p.public-key.pem')
 def get_public_key():
-    with open('/app/fleet-api/public-key.pem', 'rb') as f:
+    with open('/app/fleet_api/public/public-key.pem', 'rb') as f:
         key = f.read()
     
     return Response(content=key, media_type='application/x-pem-file')
