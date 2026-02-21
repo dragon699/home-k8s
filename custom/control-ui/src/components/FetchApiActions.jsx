b69b8dc0a09eeaca5c86bf4a606a803a9674738f
Martin Stanchev <skullfighter6@gmail.com>
2026-02-21 14:12:43 +0200

    control-ui: Card redesign #12

--- custom/control-ui/src/components/FetchApiActions.jsx
+++ custom/control-ui/src/components/FetchApiActions.jsx
@@ -173,8 +173,8 @@ export default function FetchApiActions() {
         className="block w-5 h-5"
         style={{
           backgroundColor: 'currentColor',
-          WebkitMaskImage: 'url(https://i.imgur.com/qsJbxPc.png)',
-          maskImage: 'url(https://i.imgur.com/qsJbxPc.png)',
+          WebkitMaskImage: 'url(https://i.imgur.com/LpkS09x.png)',
+          maskImage: 'url(https://i.imgur.com/LpkS09x.png)',
           WebkitMaskSize: 'contain',
           maskSize: 'contain',
           WebkitMaskRepeat: 'no-repeat',
@@ -308,7 +308,8 @@ export default function FetchApiActions() {
               target="_blank"
               rel="noreferrer"
               aria-label="Open qBittorrent"
-              className="inline-flex items-center justify-center w-8 h-8 rounded-md text-gray-400 hover:bg-gray-100 transition-colors"
+              className="inline-flex items-center justify-center w-8 h-8 rounded-md hover:bg-gray-100 transition-colors"
+              style={{ color: jellyfinAccent }}
             >
               <svg className="w-5 h-5" fill="none" stroke="currentColor" strokeWidth={2} viewBox="0 0 24 24">
                 <path strokeLinecap="round" strokeLinejoin="round" d="M4 16v2a2 2 0 002 2h12a2 2 0 002-2v-2M7 10l5 5 5-5M12 15V3" />
